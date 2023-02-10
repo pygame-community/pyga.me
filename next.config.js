076d008af85d5fde46644427be2d2eb932338cd4
Andrew Hong <andrew.jwn.hong@gmail.com>
2023-02-09 21:37:47 -0500

    Temporary upload docs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: '/docs',
+        destination: '/docs/index.html',
+        locale: false,
+      },
+    ]
+  }
 }
 
 module.exports = nextConfig
