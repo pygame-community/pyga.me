ac96298dab390fdcecdc8d4a8f25e381c0856f63
Andrew Hong <andrew.jwn.hong@gmail.com>
2023-02-10 22:51:46 -0500

    Add disclaimer

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -7,6 +7,7 @@ export default function Hero() {
             <p><u><em>The</em></u> Python</p>
             <p className={styles.frameworkgradient}>game framework</p>
             <p>since the year 2000</p>
+            <div className={styles.disclaimer}>*updated several times since then</div>
         <code className={styles.pipinstall}>pip install pygame-ce</code>
       </div>
       <div className={styles.showcasecontainer}>
