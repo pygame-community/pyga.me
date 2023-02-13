f2551f8afe76541dfa9d8275baf1155b18ede825
Andrew Hong <andrew.jwn.hong@gmail.com>
2023-02-13 03:39:35 -0500

    Add get involved buttons

--- src/components/get-involved.tsx
+++ src/components/get-involved.tsx
@@ -1,4 +1,5 @@
 import styles from "@/styles/get-involved.module.css";
+import Button from '@/components/button'
 
 export default function GetInvolved() {
   return (
@@ -15,6 +16,14 @@ export default function GetInvolved() {
                 Join the global team behind the most popular Python
                 game framework. Ranging from documentation revisions
                 to introducing new features to the pygame API.
+                <div style={{display: "flex", marginTop: '10px'}}>
+                  <Button 
+                  text="GitHub" fontSize="1.2rem"
+                  link="https://github.com/pygame-community"/>
+                  <Button
+                  text="Discord" fontSize="1.2rem"
+                  link="https://discord.gg/pygame"/>
+                </div>
             </div>
         </div>
     </div>
