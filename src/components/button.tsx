893963d7e51157e264585fe45fc70afecbf4aaad
Andrew Hong <andrew.jwn.hong@gmail.com>
2023-02-13 03:39:24 -0500

    Add button

---
+++ src/components/button.tsx
@@ -0,0 +1,26 @@
+import styles from "@/styles/button.module.css"
+import React from "react";
+
+interface ButtonProps {
+    text: string
+    fontSize: string
+    link: string
+}
+
+export default class Button extends React.Component<ButtonProps, any> {
+    constructor(props: ButtonProps) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <a href={this.props.link}>
+                <div className={styles.button}>
+                    <div style={{fontSize: this.props.fontSize}}>
+                    {this.props.text}
+                    </div>
+                </div>
+            </a>
+        )
+    }
+}
