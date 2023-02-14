64f7ca925e61529d2544cb9936046b2405977315
Andrew Hong <andrew.jwn.hong@gmail.com>
2023-02-14 11:12:34 -0500

    Remove the year

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -11,8 +11,7 @@ export default function Hero() {
           Python
         </p>
         <p className={styles.frameworkgradient}>game framework</p>
-        <p>since the year 2000</p>
-        <div className={styles.disclaimer}>*updated several times since then</div>
+        <p>that everyone uses</p>
         <code className={styles.pipinstall}>pip install pygame-ce</code>
       </div>
       <div className={styles.showcasecontainer}>
