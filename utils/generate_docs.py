1e4d382128d872483c3aa8eae4f00a8f65c735fb
Andrew Hong <andrew.jwn.hong@gmail.com>
2023-02-15 00:59:41 -0500

    Add file header

--- utils/generate_docs.py
+++ utils/generate_docs.py
@@ -1,3 +1,11 @@
+__author__ = "Andrew Hong"
+"""utils/generate_docs.py
+This file is __not__ meant to be used by a human. This was created
+to automate the process of downloading, cloning, and deploying the
+pygame documentation to the main website using  GitHub Actions. If
+you managed to run this very file, please discard your changes and
+do not do it again.
+"""
 import pygame
 import shutil
 import os
