400f99659cda7567a0a36c7abb62624941d8003c
Andrew Hong <andrew.jwn.hong@gmail.com>
2023-02-16 00:31:20 -0500

    Add a responsive navigation bar

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -1,22 +1,34 @@
+import React, { useState } from 'react';
 import styles from '@/styles/nav-bar.module.css';
 
 export default function Navbar() {
+  const [isOpen, setOpen] = useState(true);
+
+  function handleMenuClick() {
+    setOpen(!isOpen);
+  }
+
   return (
     <div className={styles.nav}>
       <div className={styles.title}>
         <a href="">pygame</a>
       </div>
-      <ul>
-        <li>
-          <a href="docs">Documentation</a>
-        </li>
-        <li>
-          <a href="tutorials">Tutorials</a>
-        </li>
-        <li>
-          <a href="https://github.com/pygame-community/pygame-ce">Contribute</a>
-        </li>
-      </ul>
+      <div className={styles.mobilemenuicon} onClick={handleMenuClick}>
+        <span className="material-symbols-outlined">menu</span>
+      </div>
+      <div className={isOpen ? styles.routes : styles.mobileroutes}>
+        <ul>
+          <li>
+            <a href="docs">Documentation</a>
+          </li>
+          <li>
+            <a href="tutorials">Tutorials</a>
+          </li>
+          <li>
+            <a href="https://github.com/pygame-community/pygame-ce">Contribute</a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,600,0,0&display=swap"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
