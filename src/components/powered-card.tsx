1dd57dad6e00501d442d035ee58f24b3bab2f710
Andrew Hong <andrew.jwn.hong@gmail.com>
2023-04-14 20:27:37 -0400

    Revert "Push new inprogress pygame powered section"
    This reverts commit 64c831bdc9dc7ff27eb31d92a843259ffb13381a.

--- src/components/powered-card.tsx
+++ src/components/powered-card.tsx
@@ -1,9 +1,8 @@
 import styles from '@/styles/powered-card.module.css';
-import Image from 'next/image';
 
 export default function PoweredCard(props: any) {
   return (
-    <div className={styles.card} style={{ backgroundImage: `url(${props.image})` }}>
+    <div className={styles.card}>
       <a href={props.link}>
         <div className={styles.name}>{props.name}</div>
         <div className={styles.author}>{props.author}</div>
